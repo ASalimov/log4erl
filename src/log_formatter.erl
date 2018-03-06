03b35c427304fb1f5f84597c6ad95035c06f526e
mac <salimov.artem@gmail.com>
2018-03-06 15:21:06 +0200

    some fix

--- src/log_formatter.erl
+++ src/log_formatter.erl
@@ -162,7 +162,8 @@ get_token_value(log, Log) ->
     Msg = Log#log.msg,
     Data = Log#log.data,
     try
-      io_lib:format(Msg, Data)
+      lager_format:format(Msg, Data,100)
+%%      io_lib:format(Msg, Data)
     catch E:R->
       error_logger:error_msg("log4erl, io_lib:format error ~p ~p ~p ~p", [E,R, Msg, Data]),
       []
